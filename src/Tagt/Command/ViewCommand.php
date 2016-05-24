a37bfd592ef9c2a847aaf15583eb033a49938ceb
Kevin McCulloch <kevin_mcculloch@ao.uscourts.gov>
2016-05-24 15:07:23 -0400

    stubbing out view command

--- src/Tagt/Command/ViewCommand.php
+++ src/Tagt/Command/ViewCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Cpyree\Id3\Metadata\Id3Metadata;
 
 /**
  * Command object for tagt view.
@@ -15,11 +16,18 @@ class ViewCommand extends TagtCommand
 {
     protected function configure()
     {
-        $this->setDescription('View ID3 tags for MP3s in the staging area');
+        $this
+            ->setDescription('View ID3 tags for MP3s in the staging area')
+            ->addArgument('file', InputArgument::REQUIRED, 'Directory')
+            ->addOption('target', 't', InputOption::VALUE_REQUIRED, 'Specify a name for the target directory', null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Stage directory: '.$this->getParameter('stage_dir'));
+        $file = $input->getArgument('file');
+        $file = getcwd().'/'.$file;
+        $output->writeln($file);
+        $id3Metadata = new Id3Metadata($file);
+        $output->writeln($id3Metadata->getTitle());
     }
 }
